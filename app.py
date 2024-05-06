4d2d0e0092eb02f75a0251c1139f9022bef7b217
Matheus Lyra <128834013+Matheusinho777@users.noreply.github.com>
2024-05-06 01:53:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ def buscar_dados_tabela():
         with connection.cursor() as cursor:
             cursor.execute(query)
             results = cursor.fetchall()
+            print(results)  # Adicionando print para verificar os dados
         return results
     finally:
         connection.close() 
@@ -26,7 +27,7 @@ def buscar_dados_tabela():
 @app.route('/')
 def index():
     dados_tabela = buscar_dados_tabela()
-    return render_template('pagina1.html', dados_tabela=dados_tabela)
+    return render_template('index.html', dados_tabela=dados_tabela)
 
 if __name__ == '__main__':
     app.run(debug=True)
