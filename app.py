e22373f3ee1a42d3d5b6c74409d8beb43736f9f8
Matheus Lyra <128834013+Matheusinho777@users.noreply.github.com>
2024-05-19 12:07:27 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,38 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from crud.py import Atleta, CadastroAtletas
 import pymysql
 
 app = Flask(__name__)
 
-db_config = {
-    'host': 'mysql-2480cbc6-iesb-pi2a2024.h.aivencloud.com',
-    'user': 'avnadmin',
-    'password': 'AVNS_FhpJunTAM7Hz3pU4pIM',
-    'database': 'defaultdb',
-    'charset': 'utf8mb4',
-    'port': 22150,
-    'cursorclass': pymysql.cursors.DictCursor
-}
+cadastro = CadastroAtletas()
 
-def buscar_dados_tabela():
-    connection = pymysql.connect(**db_config)  
-    try:
-        query = "SELECT * FROM classificacao WHERE colocacao > 0 ORDER BY CAST(colocacao AS UNSIGNED) ASC"
-        with connection.cursor() as cursor:
-            cursor.execute(query)
-            results = cursor.fetchall()
-            return jsonify(results)  # Retorna os dados da tabela no formato JSON
-    finally:
-        connection.close()
+# Conexão com o banco de dados
+connection = pymysql.connect(
+    charset="utf8mb4",
+    cursorclass=pymysql.cursors.DictCursor,
+    db="defaultdb",
+    host="mysql-2480cbc6-iesb-pi2a2024.h.aivencloud.com",
+    password="AVNS_FhpJunTAM7Hz3pU4pIM",
+    port=22150,
+    user="avnadmin",
+)
 
 @app.route('/')
 def index():
-    return buscar_dados_tabela()
+    return render_template('index.html')
+
+@app.route('/cadastro', methods=['GET', 'POST'])
+def cadastro():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        idade = int(request.form['idade'])
+        nacionalidade = request.form['nacionalidade']
+        colocacao = request.form['colocacao']
+        novo_atleta = Atleta(nome, idade, nacionalidade, colocacao)
+        cadastro.adicionar_atleta(novo_atleta)
+        cadastro.salvar_atletas(connection)
+        return redirect(url_for('index'))
+    return render_template('cadastro.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
