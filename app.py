cad0bfd58fa446b861c888e0251e97051d8f877c
Matheus Lyra <128834013+Matheusinho777@users.noreply.github.com>
2024-05-20 15:14:24 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pymysql
 
 app = Flask(__name__)
+CORS(app)  # Adicionar suporte a CORS
 
 def get_db_connection():
     connection = pymysql.connect(
